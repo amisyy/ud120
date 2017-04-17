7c0a16baf7bb6f33ca979ed789a67d19bbab7a07
amisyy <amisyy@sina.cn>
2017-04-17 16:35:19 +0800

    first commit

---
+++ ud120-projects-master/datasets_questions/explore_enron_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+""" 
+    Starter code for exploring the Enron dataset (emails + finances);
+    loads up the dataset (pickled dict of dicts).
+
+    The dataset has the form:
+    enron_data["LASTNAME FIRSTNAME MIDDLEINITIAL"] = { features_dict }
+
+    {features_dict} is a dictionary of features associated with that person.
+    You should explore features_dict as part of the mini-project,
+    but here's an example to get you started:
+
+    enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
+    
+"""
+
+import pickle
+print('start')
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+print('end')
+
+print(enron_data)
+number = 0
+for key in enron_data:
+    print(enron_data[key])
+
+print(number)
